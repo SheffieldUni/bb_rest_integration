f9eb13ff051274641f65fd61ac23b073f5e59ec9
Tom Salyers <t.salyers@sheffield.ac.uk>
2018-10-05 11:08:54 +0100

    Refactored the OAuth and XML->JSON stuff into separate modules.

--- mole_integration.py
+++ mole_integration.py
@@ -1,54 +1,16 @@
-from flask import Flask, request, Response
-#from flask_sqlalchemy import SQLAlchemy
-import datetime
-from datetime import timedelta
+from flask import Flask, request
 import requests
-from oauthlib.oauth2 import BackendApplicationClient
-from requests_oauthlib import OAuth2Session
-from requests.auth import HTTPBasicAuth
-import xmltodict 
-import json
-
+from oauth import get_auth_headers
+from request_utilities import xml_to_json
+from config import BASE_URL
 #TODO: Replace this with a better cache. This most likely isn't thread-safe. 
 # Memcache? Filesystem? UWSGI?
 from werkzeug.contrib.cache import SimpleCache
 
-# Initialize our app, secret key, configuration, and database. 
+# Initialize our app, secret key, and cache. 
 app = Flask(__name__)
-app.secret_key = "I_am_a_long_random_string_of_characters_not_really_change_me"
-app.config.from_pyfile('config.py')
-#db = SQLAlchemy(app)
+#app.secret_key = "I_am_a_long_random_string_of_characters_not_really_change_me"
 cache = SimpleCache()
-
-# Where all the magic translation happens. 
-# Depends on a generic top level XML element
-# in the request body--currently 'data'.
-# TODO: Make that configurable? 
-def xml_to_json(xml):
-	dict = xmltodict.parse(xml)
-	return json.dumps(dict['data'])
-
-# Helper function to put together the headers for requests to MOLE. 
-def get_auth_headers():
-	return {'Authorization': 'Bearer ' + get_auth_token(),'Content-Type': 'application/json'}
-	
-
-# Get a cached OAuth2 token, or a new one from MOLE if the current token's expired. 
-# TODO: Replace caching mechanism. See above re: SimpleCache.
-def get_auth_token():
-	access_token = cache.get('access_token')
-	if access_token is None:
-		print('Getting new token.')
-		auth = HTTPBasicAuth(app.config['OAUTH_CLIENT_ID'], app.config['OAUTH_SECRET'])
-		client = BackendApplicationClient(client_id=app.config['OAUTH_CLIENT_ID'])
-		oauth = OAuth2Session(client=client)
-		token = oauth.fetch_token(token_url=app.config['OAUTH_URL'], auth=auth)
-		access_token = token['access_token']
-		cache.set('access_token', access_token, timeout=3600)
-	else:
-		print('Using cached token.')
-		
-	return access_token
 
 
 # ----------- Routes	
@@ -56,9 +18,5 @@ def get_auth_token():
 # TODO: Store the rest of the route ('users') in a database table or move it to the config file.
 @app.route('/user/create', methods=['POST'])
 def create_user():
-	r = requests.post(app.config['BASE_URL'] + 'users', headers=get_auth_headers(), data=xml_to_json(request.data))
+	r = requests.post(BASE_URL + 'users', headers=get_auth_headers(cache), data=xml_to_json(request.data))
 	return str(r.status_code)
-
-	
-if __name__ == "__main__":
-	app.run()
