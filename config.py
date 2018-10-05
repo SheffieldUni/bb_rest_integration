0b6d7f92005a7600c87f79ed565fd11020ee9b78
Tom Salyers <t.salyers@sheffield.ac.uk>
2018-10-05 11:08:19 +0100

    Added settings for OAuth token timeout and the root element for XML data.

--- config.py
+++ config.py
@@ -2,3 +2,5 @@ BASE_URL = 'https://sheffield-testmig2.blackboard.com/learn/api/public/v1/'
 OAUTH_URL = 'https://sheffield-testmig2.blackboard.com/learn/api/public/v1/oauth2/token'
 OAUTH_CLIENT_ID = 'f973961c-e6bb-4485-859e-c1a6e578d554'
 OAUTH_SECRET = 'fFNs2cB69giOptspx0rhrNcJ3w7lv0mq'
+OAUTH_TIMEOUT = 3600
+DATA_ELEMENT = 'data'
