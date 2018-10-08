33dfdf15d97b7a897b61ad57c579897bb839f967
Tom Salyers <t.salyers@sheffield.ac.uk>
2018-10-08 14:22:38 +0100

    Moved cache code entirely into oauth.py where it should live--it didn't really have much business being in the main app.

--- mole_integration.py
+++ mole_integration.py
@@ -1,21 +1,14 @@
 from flask import Flask, request, Response
 import requests
 from oauth import get_auth_headers
-from request_utilities import xml_to_json
+from text_utilities import xml_to_json
 from config import BASE_URL, API_KEY
-#TODO: Replace this with a better cache. This most likely isn't thread-safe. 
-# Memcache? Filesystem? UWSGI?
-from werkzeug.contrib.cache import SimpleCache
 
-# Initialize our app, secret key, and cache. 
+# Initialize our app.
 app = Flask(__name__)
-#app.secret_key = "I_am_a_long_random_string_of_characters_not_really_change_me"
-cache = SimpleCache()
 
 
-# ----------- Routes	
-
-# Check to see if our caller has sent along an API key.
+# Security check to see if our caller has sent along an API key.
 # Without this, anyone could call these functions. 
 @app.before_request
 def check_authorization():
@@ -23,24 +16,32 @@ def check_authorization():
 		return Response(status=403)
 
 
+# ----------- Routes	
+#
+# Everything below here is basically a wrapper around Blackboard's REST API endpoints. 
+# Why? Because SITS doesn't currently speak OAuth2 or JSON, and MOLE needs both of those
+# when you're calling the API. What these do is 1.) get an OAuth2 access token (or
+# use a cached one), 2.) build the authorization headers for talking to MOLE, and 3.) transform
+# the XML we'll be getting into JSON. It should be relatively quick, but we'll see. 
+
 @app.route('/users', methods=['POST'])
 def create_user():
-	r = requests.post(BASE_URL + request.path, headers=get_auth_headers(cache), data=xml_to_json(request.data))
+	r = requests.post(BASE_URL + request.path, headers=get_auth_headers(), data=xml_to_json(request.data))
 	return str(r.status_code)
 
 
 @app.route('/users/userName:<userId>', methods=['DELETE'])
 def delete_user(userId):
-	r = requests.delete(BASE_URL + request.path, headers=get_auth_headers(cache), data=xml_to_json(request.data))
+	r = requests.delete(BASE_URL + request.path, headers=get_auth_headers(), data=xml_to_json(request.data))
 	return str(r.status_code)
 	
 @app.route('/courses/courseId:<courseId>/users/userName:<userId>', methods=['PUT'])
 def enrol_user(courseId, userId):
-	r = requests.put(BASE_URL + request.path, headers=get_auth_headers(cache), data=xml_to_json(request.data))
+	r = requests.put(BASE_URL + request.path, headers=get_auth_headers(), data=xml_to_json(request.data))
 	return str(r.status_code)
 	
 @app.route('/courses/courseId:<courseId>/users/userName:<userId>', methods=['PATCH'])	
 def update_user(courseId, userId):
-	r = requests.patch(BASE_URL + request.path, headers=get_auth_headers(cache), data=xml_to_json(request.data))
+	r = requests.patch(BASE_URL + request.path, headers=get_auth_headers(), data=xml_to_json(request.data))
 	return str(r.status_code)	
 	
--- oauth.py
+++ oauth.py
@@ -2,17 +2,17 @@ from requests.auth import HTTPBasicAuth
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 from config import OAUTH_CLIENT_ID,OAUTH_SECRET,OAUTH_URL, OAUTH_TIMEOUT
+# This Redis cache can be swapped out. 
+from werkzeug.contrib.cache import RedisCache
+cache = RedisCache()
 
-# Helper function to put together the headers for requests to MOLE. 
-# TODO: Passing the cache into this function just to pass it on to get_auth_token is
-# kind of ugly. How to rework that? 
-def get_auth_headers(cache):
-	return {'Authorization': 'Bearer ' + get_auth_token(cache),'Content-Type': 'application/json'}
+# Helper function to put together the proper headers for requests to MOLE. 
+def get_auth_headers():
+	return {'Authorization': 'Bearer ' + get_auth_token(),'Content-Type': 'application/json'}
 	
 
 # Get a cached OAuth2 token, or a new one from MOLE if the current token's expired. 
-# TODO: Replace caching mechanism. See above re: SimpleCache.
-def get_auth_token(cache):
+def get_auth_token():
 	access_token = cache.get('access_token')
 	if access_token is None:
 		print('Getting new token.')
