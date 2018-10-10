e6b2ba6b8f265aade10d11bb841f60d882b57f07
Tom Salyers <t.salyers@sheffield.ac.uk>
2018-10-10 12:53:14 +0100

    Rearranged the layout a bit and added database settings.

--- config.py
+++ config.py
@@ -1,8 +1,18 @@
+# MOLE URL and OAuth config.
 BASE_URL = 'https://sheffield-testmig2.blackboard.com/learn/api/public/v1'
 OAUTH_URL = BASE_URL + '/oauth2/token'
 OAUTH_CLIENT_ID = 'f973961c-e6bb-4485-859e-c1a6e578d554'
 OAUTH_SECRET = 'fFNs2cB69giOptspx0rhrNcJ3w7lv0mq'
 OAUTH_TIMEOUT = 3600
+
+# Database config.
+SQLALCHEMY_DATABASE_URI = 'mysql://mole_integration:mole@localhost/mole_integration'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# API key for accessing the system.
 # TODO: Make this an actual secure key before you move to production. Duh.
 API_KEY = '8675309'
+
+# Top-level element for XML bodies sent from SITS. 
 DATA_ELEMENT = 'data'
+
