cdc3513cbf370bb7ede3da699a35e00f24f2d0d7
Tom Salyers <t.salyers@sheffield.ac.uk>
2018-10-18 10:21:40 +0100

    Added error handling when getting an OAuth token.

--- oauth.py
+++ oauth.py
@@ -2,6 +2,7 @@ from requests.auth import HTTPBasicAuth
 from oauthlib.oauth2 import BackendApplicationClient
 from requests_oauthlib import OAuth2Session
 from config import OAUTH_CLIENT_ID,OAUTH_SECRET,OAUTH_URL, OAUTH_TIMEOUT
+from database_utilities import log_error
 # This Redis cache can be swapped out. 
 from werkzeug.contrib.cache import RedisCache
 cache = RedisCache()
@@ -12,18 +13,22 @@ def get_auth_headers():
 	
 
 # Get a cached OAuth2 token, or a new one from MOLE if the current token's expired. 
-# TODO: Error handling. Log getting new token?
-# Thought: catch the error, log it, return None, and let the caller error handling 
-# take care of everything. Slightly neater, possibly. 
 def get_auth_token():
 	access_token = cache.get('access_token')
 	if access_token is None:
-		print('Getting new token.')
-		auth = HTTPBasicAuth(OAUTH_CLIENT_ID, OAUTH_SECRET)
-		client = BackendApplicationClient(client_id=OAUTH_CLIENT_ID)
-		oauth = OAuth2Session(client=client)
-		token = oauth.fetch_token(token_url=OAUTH_URL, auth=auth)
-		access_token = token['access_token']
-		cache.set('access_token', access_token, timeout=OAUTH_TIMEOUT)
-		
+		try:
+			print('Getting new token.')
+			auth = HTTPBasicAuth(OAUTH_CLIENT_ID, OAUTH_SECRET)
+			client = BackendApplicationClient(client_id=OAUTH_CLIENT_ID)
+			oauth = OAuth2Session(client=client)
+			token = oauth.fetch_token(token_url=OAUTH_URL, auth=auth)
+			access_token = token['access_token']
+			cache.set('access_token', access_token, timeout=OAUTH_TIMEOUT)
+		except Exception as e:
+			# Uh-oh. Something's gone wrong. Log the error, then 
+			# return None and let our caller finish handling the 
+			# whole thing. 
+			log_error("OAuth token request", str(e), 500, '')
+			access_token = None
+			
 	return access_token
