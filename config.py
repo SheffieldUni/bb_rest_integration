ff09a50681e46ad78a7c52654d31b172d1b68590
Tom Salyers <t.salyers@sheffield.ac.uk>
2018-10-25 13:21:14 +0100

    Missed updating the OAuth endpoint when I updated the others.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # MOLE URL and OAuth config.
 BASE_URL = 'https://sheffield-testmig2.blackboard.com/learn/api/public'
-OAUTH_URL = BASE_URL + '/oauth2/token'
+OAUTH_URL = BASE_URL + '/v1/oauth2/token'
 OAUTH_CLIENT_ID = 'f973961c-e6bb-4485-859e-c1a6e578d554'
 OAUTH_SECRET = 'fFNs2cB69giOptspx0rhrNcJ3w7lv0mq'
 OAUTH_TIMEOUT = 3600
