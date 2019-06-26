b3fbd3e4cb8e55da1f665c936acc5df919b63e6d
Tom Salyers <T.Salyers@sheffield.ac.uk>
2019-06-26 15:02:12 +0100

    Temporary commit--cleared sensitive data.

--- config.py
+++ config.py
@@ -2,18 +2,17 @@
 # TODO: When we go live, replace these keys and don't check them into Git. 
 BASE_URL = 'https://vle-staging.sheffield.ac.uk/learn/api/public'
 OAUTH_URL = BASE_URL + '/v1/oauth2/token'
-OAUTH_CLIENT_ID = '1509cc57-d0a9-47e7-b622-c04637bb3fbc'
-OAUTH_SECRET = '1WyvdgV9ZcZwZFzP3vQxUxsUUAtOFUPN'
+OAUTH_CLIENT_ID = ''
+OAUTH_SECRET = ''
 OAUTH_TIMEOUT = 3600
 
 # Database config.
-SQLALCHEMY_DATABASE_URI = 'mysql://mole_integration:mole@localhost/mole_integration'
+SQLALCHEMY_DATABASE_URI = 'mysql://user:pass@localhost/mole_integration'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 TRANSACTION_LOGGING = True
 
 # API key for accessing the system.
-# TODO: Make this an actual secure key before you move to production. Duh.
-API_KEY = '8675309'
+API_KEY = ''
 
 # Top-level element for XML bodies sent from SITS.
 DATA_ELEMENT = 'data'
