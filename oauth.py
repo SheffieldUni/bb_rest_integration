f5f8cb08ab047e09e9096f067e16e4a732af63a6
Tom Salyers <t.salyers@sheffield.ac.uk>
2019-08-28 11:12:30 +0100

    Replaced the token generation code with something a little simpler that doesn't rely on a lot of external libraries.

--- oauth.py
+++ oauth.py
@@ -1,9 +1,11 @@
-from requests.auth import HTTPBasicAuth
-from oauthlib.oauth2 import BackendApplicationClient
-from requests_oauthlib import OAuth2Session
+import requests
+from base64 import b64encode
+import json
 from config import OAUTH_CLIENT_ID, OAUTH_SECRET, OAUTH_URL, OAUTH_TIMEOUT
 # This Redis cache can be swapped out with other supported caches. 
 from werkzeug.contrib.cache import RedisCache
+
+# Initialize our cache.
 # TODO: Set up password authentication for some extra security. 
 cache = RedisCache(default_timeout=OAUTH_TIMEOUT)
 
@@ -17,12 +19,18 @@ def get_auth_token():
 	access_token = cache.get('access_token')
 	if access_token is None:
 		try:
-			print('Getting new token.')
-			auth = HTTPBasicAuth(OAUTH_CLIENT_ID, OAUTH_SECRET)
-			client = BackendApplicationClient(client_id=OAUTH_CLIENT_ID)
-			oauth = OAuth2Session(client=client)
-			token = oauth.fetch_token(token_url=OAUTH_URL, auth=auth)
-			access_token = token['access_token']
+			# First encode our credentials. The standard expects a base64-encoded string of the 
+			# key and secret separated by a colon--'key:secret'. The encode and decode calls 
+			# are because we need to send a string, but b64encode works on bytes. Irritating.
+			encoded_credentials = b64encode( (OAUTH_CLIENT_ID + ':' + OAUTH_SECRET).encode() ).decode()
+			
+			# Add 'Basic' on to form the rest of our header content, then post our request to the 
+			# auth URL. Note the grant_type body field--this has to be set or the request won't work.
+			authorization = 'Basic ' + encoded_credentials
+			resp = requests.post(OAUTH_URL, headers={'Authorization':authorization}, data={'grant_type':'client_credentials'})
+			
+			#Finally, grab the access token from the returned JSON and cache it. 
+			access_token = json.loads(resp.text)['access_token']
 			cache.set('access_token', access_token)
 		except Exception as e:
 			# Uh-oh. Something's gone wrong. Re-raise our exception.
